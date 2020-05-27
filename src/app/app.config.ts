283ae58c82518dae786e4da2df3267c3016ab110
Stephen Smith <itcombox@gmail.com>
2020-05-27 21:15:20 +0100

    - make login redirect URI context sensistive to current domain

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,7 +2,7 @@ export default {
     oidc: {
         clientId: `${'0oabrzq86or1DRdVO4x6'}`,
         issuer: `${'https://dev-603947.okta.com/oauth2/default'}`,
-        redirectUri: 'http://localhost:4200/implicit/callback',
+        redirectUri: `${location.origin}/implicit/callback`,
         scopes: ['openid', 'profile', 'email'],
         testing: {
             disableHttpsCheck: `${true}`
