17fc6e2c4445404512c23edfe58ba42cd0725751
Stephen Smith <itcombox@gmail.com>
2020-06-19 07:24:37 +0100

    - add shorter routing path for commercial section - fix issue with image slider for desktop viewports

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -36,6 +36,11 @@ const routes: Routes = [
     data: { section: 'projects', contentpath: 'commercial' }
   },
   {
+    path: 'commercial',
+    component: ContentProviderComponent,
+    data: { section: 'projects', contentpath: 'commercial' }
+  },
+  {
     path: 'projects/other',
     component: ContentProviderComponent,
     data: { section: 'projects', contentpath: 'other' }
